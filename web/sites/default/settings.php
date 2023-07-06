a906d42e35425b29bb5373fda6b62a16f347613e
DDEV User <nobody@example.com>
2023-07-06 13:51:26 +0000

    Update to the latest version of D8

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -66,3 +66,9 @@ $settings['install_profile'] = 'minimal';
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
     include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = dirname(__FILE__) . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
